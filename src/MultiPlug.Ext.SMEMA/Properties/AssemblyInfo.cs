3c142626cfa69d063f8c17a62f68cbfaf4393507
4IR.UK <hello@industry4.uk>
2023-07-03 01:40:27 +0100

    Release 1.0.14

--- src/MultiPlug.Ext.SMEMA/Properties/AssemblyInfo.cs
+++ src/MultiPlug.Ext.SMEMA/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("4IR.UK British Systems")]
 [assembly: AssemblyProduct("SMEMA")]
-[assembly: AssemblyCopyright("4IR.UK British Systems 2022")]
+[assembly: AssemblyCopyright("4IR.UK British Systems 2023")]
 [assembly: AssemblyTrademark("MultiPlug")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.13.0")]
-[assembly: AssemblyFileVersion("1.0.13.0")]
+[assembly: AssemblyVersion("1.0.14.0")]
+[assembly: AssemblyFileVersion("1.0.14.0")]
